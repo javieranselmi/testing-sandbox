11aa15d935f998b7d7aebb85c9ed5eecb10a0512
javieranselmi <javieranselmi@gmail.com>
2016-12-01 12:48:25 -0300

    Added test flow until checkout page

--- tests/test.js
+++ tests/test.js
@@ -1,13 +1,8 @@
 module.exports = {
 
-  'Search \'samsung\'' : function (browser) {
-    browser
-        .url('https://staging.trocafone.com')
-        .waitForElementVisible('body', 1000)
-        .setValue('input[name=q].search-box__input', 'samsung')
-        .click('button[type=submit].search-box__button')
-        .waitForElementVisible('div.results.js-scrollable-listing', 1000)
-        .click('.js-product-box-container:first-child')
+   'partialBuy': function (browser) {
+        browser
+        .url('https://staging.trocafone.com/comprar/samsung/samsung-grand-prime-3g-duos#424-samsung-grand-prime-3g-duos-8gb-branco-bom-15')
         .waitForElementVisible('input[name=buy_submit]', 1000)
         .click('input[name=buy_submit]')
         .waitForElementVisible('input[name=buy_submit]', 1000)
@@ -16,23 +11,37 @@ module.exports = {
         .click('input[name=buy_submit]')
         .setValue('.js-checkout-name input[name=name]', 'Mockity Mocks')
         .setValue('.js-checkout-email input[name=email]', 'test_user_57812159@testuser.com')
-        .setValue('.js-checkout-birthdate input[name=birthdate]', '10/09/1990')
-        .setValue('.js-checkout-cpf input[name=cpf]', '46.276.537')
-        .setValue('.js-checkout-phone_number input[name=phone_number]', '361928845578_')
+        //.setValue('.js-checkout-birthdate input[name=birthdate]', '01-01-2015')
+        .execute(
+               function() {
+                   var birthdate = document.querySelector('.js-checkout-birthdate input[name=birthdate]');
+                   birthdate.value="01/01/2015";
+               }
+         )
+        .setValue('.js-checkout-cpf input[name=cpf]', '871.375.668-04')
+        .setValue('.js-checkout-phone_number input[name=phone_number]', '361928845578')
         .setValue('.js-checkout-gender select[name=gender]', 'Male')
-        .pause(3000)
-        .end();
-  }
+        .execute(
+            function() {
+                var cep = document.querySelector('.js-checkout-cep input[name=cep]');
+                cep.value="05415-060";
+                cep.onchange();
+            }
+        )
+        .pause(1000)
+        .waitForElementVisible('.js-checkout-number input[name=number]', 1000)
+        .setValue('.js-checkout-number input[name=number]', '12345')
+        .click('button.js-stepInformation-submit')
+        .pause(1000);
+        browser.expect.element('.checkout-box-inactive.js-delivery-input').to.not.be.visible;
+        browser
+        .click('.js-stepDelivery-submit')
+        .pause(10000)
+        .end(0);
+
+    }
 };
 
 
 
-    /*elements: {
-        searchBar: {
-            selector: 'input[type=text]'
-        },
-        submit: {
-            selector: '//[@name="q"]',
-            locateStrategy: 'xpath'
-        }
-    }*/
+
